aabe7918991664e7d25f583361e2003ffcb8efdf
Lárus Sverrisson <32623302+larussverris@users.noreply.github.com>
2019-02-08 13:45:51 +0000

    Update main.py

--- skil3/main.py
+++ skil3/main.py
@@ -5,7 +5,7 @@ import itertools
 
 def stafrof(lengd):
     stafir = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    listi = list(itertools.combinations(stafir, 3))
+    listi = list(itertools.combinations(stafir, lengd))
     for x in listi:
         print(*x)
         
