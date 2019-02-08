c4ad3e1658223364b4196e461be952ece80a0239
Lárus Sverrisson <32623302+larussverris@users.noreply.github.com>
2019-02-08 14:14:50 +0000

    Update main.py

--- skil3/main.py
+++ skil3/main.py
@@ -1,13 +1,50 @@
-import itertools
+from itertools import *
 
-#skil3 Lárus Arnar
-#allar mögulegir strengir af lengd n úr enska stafrófinu
+staf = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+def strengur(n):
+    listi = []
+    l = list(combinations(staf, n))
+    for a in l:
+        s = ""
+        for x in a:
+            s += x
+        listi.append(s)
 
-def stafrof(lengd):
-    stafir = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    listi = list(itertools.combinations(stafir, lengd))
-    for x in listi:
-        print(*x)
-        
-        
-#stafrof(3)
+    return listi
+
+def bubble(items):
+
+    while True:
+        c = 0
+        p = 1
+        swap = False
+        for a in range(len(items)-1):
+            if items[c] > items[p]:
+                swap = True
+                items[c], items[p] = items[p], items[c]
+
+
+            c += 1
+            p += 1
+
+        if swap is False:
+            break
+
+
+    return items
+
+p = strengur(3)
+for a in p:
+    print(a)
+input("Enter to continue")
+print("----------------------------")
+p.reverse()
+print(p)
+input("Enter to continue")
+print("----------------------------")
+p = bubble(p)
+print(p)
+input("Enter to continue")
+
+
+
